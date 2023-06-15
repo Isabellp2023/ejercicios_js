95bce678541bba7bf356fd159e410888c04c7406
UsuarioM <isabelopez.cursos@gmail.com>
2023-06-15 13:42:20 +0200

    feat ejercici8.js terminado

---
+++ ejercicio8.js
@@ -0,0 +1,17 @@
+/*Ejercicio [8]
+Completa la función que tomando dos números como argumento devuelva el más alto.*/
+/*function sum(numberOne , numberTwo) {
+    // insert code*/
+
+    function sum (numberOne , numberTwo) {
+        if (numberOne > numberTwo) {
+        return numberOne; 
+       } else {
+       return numberTwo;  
+        }
+        
+       
+       }
+   
+       console.log(sum(6 ,8));
+   
