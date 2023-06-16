a00d6b06e92482776aa77e06a54082a366df0ab0
UsuarioM <isabelopez.cursos@gmail.com>
2023-06-16 14:11:27 +0200

    feat ejercicio corregido

--- ejercicio9.js
+++ ejercicio9.js
@@ -2,7 +2,16 @@
 caso de que dos strings tenga la misma longitud deberá devolver el primero.
 Puedes usar este array para probar tu función:*/
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-function findLongestWord(param) {
+function findLongestWord(list) {
+let longestWord = "";
+for (const item of list){
+    if (item.length > longestWord.length){
+        longestWord = item;
 
+    }
+}
+console.log ("La palabra mas larga es : " + longestWord);
 
-    
+}
+findLongestWord(avengers); 
+
